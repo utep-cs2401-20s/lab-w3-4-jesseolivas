55d431b8ecc3a97fa30c7097f1e9855c05c14861
jesseolivas <jesseolivas@live.com>
2020-02-05 22:32:58 -0700

    Completed oneStep method, needs testing.

--- lab3/src/GameOfLife.java
+++ lab3/src/GameOfLife.java
@@ -3,6 +3,12 @@ public class GameOfLife{
     int[][] board;
     int[][] previous;
 
+    // RULES:
+    //  Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    //  Any live cell with two or three live neighbours lives on to the next generation.
+    //  Any live cell with more than three live neighbours dies, as if by overpopulation.
+    //  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
     public GameOfLife() {
     }
 
@@ -25,7 +31,6 @@ public class GameOfLife{
                 previous[i][j] = b[i][j];
             }
         }
-
     }
 
     // Returns the current board
@@ -34,7 +39,32 @@ public class GameOfLife{
     }
 
     // FIX ME
+    // Takes no parameter, void method
+    // Transforms the current board into its next shape
     public void oneStep(){
+        int[][] current = new int[size][size];
+        for (int i = 0;i < current.length; i++){
+            for(int j = 0; j < current[i].length; j++){
+                current[i][j] = previous[i][j];
+            }
+        }
+        for (int i = 0;i < current.length; i++){
+            for(int j = 0; j < current[i].length; j++){
+                if(current[i][j] == 1) {
+                    if (neighbors(current[i][0], current[0][j]) < 2) {
+                        current[i][j] = 0;
+                    }
+                    else if (neighbors(current[i][0], current[0][j]) > 3) {
+                        current[i][j] = 0;
+                    }
+                }
+                else {
+                    if (neighbors(current[i][0], current[0][j]) == 3) {
+                        current[i][j] = 1;
+                    }
+                }
+            }
+        }
 
     }
 
@@ -71,6 +101,9 @@ public class GameOfLife{
     }
 
     // FIX ME
+    // Takes int n, number of evolution steps needed to conduct
+    // Transforms the board into the board after n steps of evolution
+    // (n successive calls to oneStep)
     public int[][] evolution(int n){
         return board;
     }
